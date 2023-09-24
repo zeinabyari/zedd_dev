262b99d17a0dab3eb33e74038df0d369c6d3f7cb
zeinab yari <zeiinabyarii@gmail.com>
2023-09-24 12:19:48 +0330

    put asset in assets func

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -6,17 +6,22 @@
             <!-- Widget 1 START -->
             <div class="col-lg-3">
                 <!-- logo -->
-                <a class="me-0" href="index.html">
-                    <img class="light-mode-item h-40px" src="assets/images/logo-light.svg" alt="logo">
-                    <img class="dark-mode-item h-40px" src="assets/images/logo.svg" alt="logo">
+                <a class="me-0" href="{{ route('home') }}">
+                    <img class="light-mode-item h-40px" src="{{ asset('assets/images/logo-light.svg') }}" alt="logo">
+                    <img class="dark-mode-item h-40px" src="{{ asset('assets/images/logo.svg') }}" alt="logo">
                 </a>
-                <p class="my-3 text-muted">شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                <p class="my-3 text-muted">شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی
+                    اساسا مورد استفاده قرار گیرد.</p>
                 <!-- Social media icon -->
                 <ul class="list-inline mb-0 mt-3">
-                    <li class="list-inline-item"> <a class="btn btn-white btn-sm shadow px-2 text-facebook" href="#"><i class="fab fa-fw fa-facebook-f"></i></a> </li>
-                    <li class="list-inline-item"> <a class="btn btn-white btn-sm shadow px-2 text-instagram" href="#"><i class="fab fa-fw fa-instagram"></i></a> </li>
-                    <li class="list-inline-item"> <a class="btn btn-white btn-sm shadow px-2 text-twitter" href="#"><i class="fab fa-fw fa-twitter"></i></a> </li>
-                    <li class="list-inline-item"> <a class="btn btn-white btn-sm shadow px-2 text-linkedin" href="#"><i class="fab fa-fw fa-linkedin-in"></i></a> </li>
+                    <li class="list-inline-item"><a class="btn btn-white btn-sm shadow px-2 text-facebook" href="#"><i
+                                class="fab fa-fw fa-facebook-f"></i></a></li>
+                    <li class="list-inline-item"><a class="btn btn-white btn-sm shadow px-2 text-instagram" href="#"><i
+                                class="fab fa-fw fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a class="btn btn-white btn-sm shadow px-2 text-twitter" href="#"><i
+                                class="fab fa-fw fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a class="btn btn-white btn-sm shadow px-2 text-linkedin" href="#"><i
+                                class="fab fa-fw fa-linkedin-in"></i></a></li>
                 </ul>
             </div>
             <!-- Widget 1 END -->
@@ -74,11 +79,11 @@
                 <div class="row g-2 mt-2">
                     <!-- Google play store button -->
                     <div class="col-6 col-sm-4 col-md-3 col-lg-6">
-                        <a href="#"> <img src="assets/images/client/google-play.svg" alt=""> </a>
+                        <a href="#"> <img src="{{ asset('assets/images/client/google-play.svg') }}" alt=""> </a>
                     </div>
                     <!-- App store button -->
                     <div class="col-6 col-sm-4 col-md-3 col-lg-6">
-                        <a href="#"> <img src="assets/images/client/app-store.svg" alt="app-store"> </a>
+                        <a href="#"> <img src="{{ asset('assets/images/client/app-store.svg') }}" alt="app-store"> </a>
                     </div>
                 </div> <!-- Row END -->
             </div>
@@ -93,25 +98,38 @@
             <div class="container px-0">
                 <div class="d-lg-flex justify-content-between align-items-center py-3 text-center text-md-left">
                     <!-- copyright text -->
-                    <div class="text-muted text-primary-hover"> ارائه شده در سایت <a href="https://www.rtl-theme.com/" target="_blank" class="text-reset">راست چین</a></div>
+                    <div class="text-muted text-primary-hover"> ارائه شده در سایت <a href="https://www.rtl-theme.com/"
+                                                                                     target="_blank" class="text-reset">راست
+                            چین</a></div>
                     <!-- copyright links-->
                     <div class="justify-content-center mt-3 mt-lg-0">
                         <ul class="nav list-inline justify-content-center mb-0">
                             <li class="list-inline-item text-primary-hover">
                                 <!-- Language selector -->
                                 <div class="dropup mt-0 text-center text-sm-end">
-                                    <a class="dropdown-toggle nav-link" href="#" role="button" id="languageSwitcher" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <a class="dropdown-toggle nav-link" href="#" role="button" id="languageSwitcher"
+                                       data-bs-toggle="dropdown" aria-expanded="false">
                                         <i class="fas fa-globe me-2"></i>زبان ها
                                     </a>
                                     <ul class="dropdown-menu min-w-auto" aria-labelledby="languageSwitcher">
-                                        <li><a class="dropdown-item me-4 text-body" href="#"><img class="fa-fw me-2" src="assets/images/flags/uk.svg" alt="">فارسی</a></li>
-                                        <li><a class="dropdown-item me-4 text-body" href="#"><img class="fa-fw me-2" src="assets/images/flags/gr.svg" alt="">انگلیسی </a></li>
-                                        <li><a class="dropdown-item me-4 text-body" href="#"><img class="fa-fw me-2" src="assets/images/flags/sp.svg" alt="">فرانسوی</a></li>
+                                        <li><a class="dropdown-item me-4 text-body" href="#"><img class="fa-fw me-2"
+                                                                                                  src="{{ asset('assets/images/flags/uk.svg') }}"
+                                                                                                  alt="">فارسی</a></li>
+                                        <li><a class="dropdown-item me-4 text-body" href="#"><img class="fa-fw me-2"
+                                                                                                  src="{{ asset('assets/images/flags/gr.svg') }}"
+                                                                                                  alt="">انگلیسی </a>
+                                        </li>
+                                        <li><a class="dropdown-item me-4 text-body" href="#"><img class="fa-fw me-2"
+                                                                                                  src="{{ asset('assets/images/flags/sp.svg') }}"
+                                                                                                  alt="">فرانسوی</a>
+                                        </li>
                                     </ul>
                                 </div>
                             </li>
-                            <li class="list-inline-item text-primary-hover"><a class="nav-link" href="#">شرایط استفاده</a></li>
-                            <li class="list-inline-item text-primary-hover"><a class="nav-link pe-0" href="#">قوانین سایت</a></li>
+                            <li class="list-inline-item text-primary-hover"><a class="nav-link" href="#">شرایط
+                                    استفاده</a></li>
+                            <li class="list-inline-item text-primary-hover"><a class="nav-link pe-0" href="#">قوانین
+                                    سایت</a></li>
                         </ul>
                     </div>
                 </div>
